1d453c07b810169cf1bfdcb146ef18b29f8b0e59
Emmet Jao <emmet.jao@gmail.com>
2013-09-26 18:26:08 -0400

    cleaned up comblexer.ml

--- ps1/comblexer.ml
+++ ps1/comblexer.ml
@@ -4,9 +4,9 @@ open Lcombinators.CharParsing
 (* the datatype for tokens -- you will need to augment these *)
 type token = 
     (* types *)
-    INT of int | VAR of string | NOT | AND | OR
+    INT of int | VAR of string
     (* operators *)
-  | PLUS | MINUS | TIMES | DIV | EQ | NEQ | LT | LTE | GT | GTE | ASSIGN(* | STAR | SLASH*)
+  | PLUS | MINUS | TIMES | DIV | EQ | NEQ | LT | LTE | GT | GTE | ASSIGN | NOT | AND | OR
     (* Parens and braces *)
   | LPAREN | RPAREN | LCURLY | RCURLY
     (* non-code critical *)
@@ -21,11 +21,6 @@ type token =
 let remove_whitespace (ts: token list) : token list =
   let p = fun a t -> match t with (WHITESPACE | COMMENT) -> a | _ -> t::a in
   List.rev (List.fold_left p [] ts)
-
-(* matches an identifier *)
-let real_identifier : (char, token) parser =
-  let id_str = map Explode.implode (cons (alpha, star (alts [alpha; dig; underscore]))) in
-  map (fun i -> VAR i) id_str
 
 (* the tokenize function -- should convert a list of characters to a list of 
  * Fish tokens using the combinators. *)
@@ -52,12 +47,12 @@ let rec tokenize(cs:char list) : token list =
   (* non-code critical *)
   let ws_parser = const_map WHITESPACE white in
   let comment_parser = const_map COMMENT comment in
-  (* types *)
+  (* ints *)
   let int_parser = map (fun i -> INT i) integer in
+  (* operators *)
   let and_parser = const_map AND (str "&&") in
   let or_parser = const_map OR (str "||") in
   let not_parser = const_map NOT (c '!') in
-  (* operators *)
   let plus_parser = const_map PLUS (c '+') in
   let minus_parser = const_map MINUS (c '-')  in
   let times_parser = const_map TIMES (c '*') in
@@ -68,14 +63,13 @@ let rec tokenize(cs:char list) : token list =
   let lte_parser = const_map LTE (str "<=") in
   let gt_parser = const_map GT (c '>') in
   let gte_parser = const_map GTE (str ">=") in
-  (* ASSIGN PARSER?? *)
   let assign_parser = const_map ASSIGN (c '=') in
   (* parens and braces *)
   let lparen_parser = const_map LPAREN (c '(') in
   let rparen_parser = const_map RPAREN (c ')') in
   let lcurly_parser = const_map LCURLY (c '{') in
   let rcurly_parser = const_map RCURLY (c '}') in
-  (* control statements *)
+  (* semicolon *)
   let semi_parser = const_map SEMI (c ';') in
   let all_tokens = [int_parser; ws_parser; comment_parser; 
     plus_parser; minus_parser; times_parser; div_parser;
