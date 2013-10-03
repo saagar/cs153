86cfdd7b778a1d16e6543ab9751321e927cca088
Emmet Jao <emmet.jao@gmail.com>
2013-10-03 15:25:51 -0400

    prefix variables before adding them

--- ps2/compile.ml
+++ ps2/compile.ml
@@ -31,6 +31,11 @@ let rec new_temp() =
 (* reset internal state *)
 let reset() = (label_counter := 0; variables := VarSet.empty)
 
+(* wrapper for VarSet.add that prefixes the variable name and adds it to the variables set *)
+let mangle_add v : () =
+  let mangled_v = String.concat "" ["_sdej_"; v] in
+  variables := VarSet.add mangled_v !variables
+
 (* find all of the variables in a program and add them to
  * the set variables *)
 let rec collect_vars (p : Ast.program) : unit = 
@@ -41,12 +46,12 @@ let rec collect_vars (p : Ast.program) : unit =
     let (r, _) = e in
     match r with
       Ast.Int _ -> ()
-    | Ast.Var x -> variables := VarSet.add x !variables
+    | Ast.Var x -> mangle_add x
     | Ast.Binop (e1, _, e2) -> collect_vars_exp e1; collect_vars_exp e2
     | Ast.Not e1 -> collect_vars_exp e1
     | Ast.And (e1, e2) -> collect_vars_exp e1; collect_vars_exp e2
     | Ast.Or (e1, e2) -> collect_vars_exp e1; collect_vars_exp e2
-    | Ast.Assign (x, e1) -> variables := VarSet.add x !variables; collect_vars_exp e1 in
+    | Ast.Assign (x, e1) -> mangle_add x; collect_vars_exp e1 in
   let (s, _) = p in
   match s with
   | Ast.Exp e -> collect_vars_exp e
