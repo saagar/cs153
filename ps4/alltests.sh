c157dadd04a7706d1e83d1badaa319ba8e24d34d
Saagar Milind Deshpande <sdeshpande@college.harvard.edu>
2013-10-23 01:34:52 -0400

    added tests

---
+++ ps4/alltests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILES="test/*"
+function runall {
+  i=0
+  for f in $FILES
+  do
+    i=$(($i+1))
+    out=`./ps4_scish $f | ./ps4_cish`
+    a=( $out )
+    if [[ ${a[0]} = "Fatal" ]]; then
+      echo "boom"
+    else
+      ans=`head -$i solutions | tail -1`
+      if [[ $ans = ${a[2]} ]]; then
+        echo -n -e "\e[92m[ SUCCESS ] "
+        tput sgr0
+        echo Test $i passed! answer: ${a[2]}, expected: $ans
+      else
+        echo -n -e "\e[91m[ FAILED! ] "
+        tput sgr0
+        echo Test $i answer: ${a[2]}, expected: $ans
+      fi
+    fi
+  done
+}
+
+runall
