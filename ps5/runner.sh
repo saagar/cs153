c2624dbeb831acaf3ccd540c6d8ace88edbe68ed
Saagar Milind Deshpande <sdeshpande@college.harvard.edu>
2013-11-01 04:37:02 -0400

    fixed solutions. stat110 kids are idiots on halloween

--- ps5/runner.sh
+++ ps5/runner.sh
@@ -2,18 +2,36 @@
 
 FILES="test/*"
 
+function trim {
+  local var=$@
+  var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
+  var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
+  echo -n "$var"
+
+}
+
 function runall {
   for f in $FILES
   do
-    echo "== $f =="
     NAME=${f%.*}
     SOLN=${NAME##*/}
+    echo -e "\e[94m|=== $NAME ===|"
+    tput sgr0
     ANSWER=$(./ps5_mlish "$f")
     EXPECTED=$(cat soln/$SOLN.soln)
     if [ "$ANSWER" != "answer = $EXPECTED" ]; then
-      echo -n -e "\e[91m[ FAILED! ] "
-      tput sgr0
-      echo "$SOLN: answer: $ANSWER, expected: $EXPECTED"
+      # test failed, check error messages
+      if [ "$ANSWER" != "$EXPECTED" ]; then
+      
+        echo -n -e "\e[95m[ SUCCESS ] "
+        tput sgr0
+        echo "$SOLN: answer: $ANSWER, expected: $EXPECTED"
+      else
+        echo -n -e "\e[91m[ FAILED! ] "
+        tput sgr0
+        echo "$SOLN: answer: $ANSWER, expected: $EXPECTED"
+      fi
+
     else
       echo -n -e "\e[92m[ SUCCESS ] "
       tput sgr0
