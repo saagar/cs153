483caae9e7a6d88e4fa35cb15fd50271024d3475
Saagar Milind Deshpande <sdeshpande@college.harvard.edu>
2013-10-31 21:29:19 -0400

    unify and occurs

--- ps5/mlish_type_check.ml
+++ ps5/mlish_type_check.ml
@@ -1,15 +1,49 @@
 open Mlish_ast
 
 exception TypeError
+exception TODO
 let type_error(s:string) = (print_string s; raise TypeError)
 
 let extend (e:(var*tipe_scheme) list) (x:var) (s:tipe_scheme) : (var*tipe_scheme) list = raise TypeError
 
 let lookup (e:(var*tipe_scheme) list) (x:var) : tipe_scheme = raise TypeError
 
-let guess () : tipe = raise TypeError
+(* Check if a Guess appears in a tipe. If so, there's some recursion to avoid *)
+let rec occurs (guess:tipe option ref) (t:tipe) : bool = 
+  match t with
+  | Guess_t t1 -> guess == t1
+  | Fn_t (t1, t2) -> occurs guess t1 || occurs guess t2
+  | Pair_t (t1, t2) -> occurs guess t1 || occurs guess t2
+  | List_t t1 -> occurs guess t1
+  | _ -> false
 
-let unify (t1:tipe) (t2:tipe) : bool = raise TypeError
+(* Generate a new Guess_t *)
+let guess () : tipe = Guess_t(ref None)
+
+let rec unify (t1:tipe) (t2:tipe) : bool =
+  if (t1 = t2) then true
+  else
+    match (t1, t2) with
+    | Guess_t(t1_guess), _ -> 
+        (match !t1_guess with
+          | None ->
+              if (occurs t1_guess t2) 
+              then type_error ("Loop occurred! Recursion detected.")
+              else let _ = t1_guess := Some t2 in true
+          | Some(t1_g) -> unify t1_g t2)
+    (* flip the guess and run again *)
+    | _, Guess_t(_) -> unify t2 t1
+    (* Functions, Pairs, Lists *)
+    | Fn_t(t1a,t1b), Fn_t(t2a,t2b) -> (unify t1a t2a) && (unify t1b t2b)
+    | Pair_t(t1a,t1b), Pair_t(t2a,t2b) -> (unify t1a t2a) && (unify t1b t2b)
+    | List_t(t1a), List_t(t2a) -> unify t1a t2a
+    (* Probably can combine this - do we even get to these cases? *)
+    | Tvar_t t1a, _ -> true
+    | _, Tvar_t t2a -> true
+    | Int_t, Int_t -> true
+    | Bool_t, Bool_t -> true
+    | Unit_t, Unit_t -> true
+    | _ -> type_error("Unable to unify tipes!")
 
 let instantiate (s:tipe_scheme) : tipe = raise TypeError
 
@@ -27,19 +61,3 @@ let rec tc (env:(var*tipe_scheme) list) ((e,_):exp) : tipe =
 
 let type_check_exp (e:Mlish_ast.exp) : tipe = raise TypeError
 
-(* Generate a new Guess_t *)
-let guess () =
-  Guess_t(ref None)
-
-let rec unify (t1:tipe) (t2:tipe) : bool =
-  if (t1 = t2) then true
-  else
-    match (t1, t2) with
-    | Guess_t(t1_guess), t2 -> 
-        (
-          match !t1_guess with
-          | None ->
-              if (occurs
-          | Some(t1_g) -> unify t1_g t2
-        )
-    | (t1, 
