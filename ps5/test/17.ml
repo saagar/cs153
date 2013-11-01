da2c79fa7b03b0466de89c3fb6f5cd3f20267329
Saagar Milind Deshpande <sdeshpande@college.harvard.edu>
2013-11-01 04:52:11 -0400

    shadowing

---
+++ ps5/test/17.ml
@@ -0,0 +1,9 @@
+let x = 5 in
+let x = 6 in
+let x = 7 in
+let x = 8 in
+let y = 10 in
+let y = 24 in
+let y = 49 in
+let y = 64 in
+y / x
---
+++ ps5/test/18.ml
@@ -0,0 +1,3 @@
+let x = 5 in
+let y = (let x = 15 in x + 1) in
+y + x
